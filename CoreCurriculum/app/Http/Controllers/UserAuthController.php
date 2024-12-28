4ddcd4b88c28f2126195c66021060c713c5ddd63
Emerson <emersonpedrosacontato@gmail.com>
2024-12-28 12:31:12 -0300

    Cadastro de users feito

--- CoreCurriculum/app/Http/Controllers/UserAuthController.php
+++ CoreCurriculum/app/Http/Controllers/UserAuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
 {
@@ -15,8 +17,14 @@ class UserAuthController extends Controller
     public function returnPageCadastro(){
         return view('UserAuth.cadastro');
     }
-    public function PostCadastro(){
-        return "post cadastro";
+    public function PostCadastro(Request $request){
+
+        $modelUser = new User();
+        $modelUser->name = $request->input('name');
+        $modelUser->email = $request->input('email');
+        $modelUser->password = Hash::make($request->input('password'));
+        $modelUser->save();
+        return redirect("loginPage");
     }
     public function PostLogin(){
         return "post login";
